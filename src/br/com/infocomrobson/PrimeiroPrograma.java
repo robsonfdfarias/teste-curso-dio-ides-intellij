a6d1415c20b19b925a5c6b6dbcd51c3b0be66805
robsonfdfarias <robsonfdfarias@gmail.com>
2022-06-08 17:49:59 -0300

    first commit

---
+++ src/br/com/infocomrobson/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.infocomrobson;
+
+import br.com.infocomrobson.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int a = 5;
+        int b = 13;
+        System.out.println("Hello world! "+(a+b));*/
+    }
+}
