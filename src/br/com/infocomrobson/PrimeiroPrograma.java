2b607d18378848eaa22134a224800c6867ad86b5
robsonfdfarias <robsonfdfarias@gmail.com>
2022-06-08 17:59:00 -0300

    Criando class Livro

--- src/br/com/infocomrobson/PrimeiroPrograma.java
+++ src/br/com/infocomrobson/PrimeiroPrograma.java
@@ -4,10 +4,49 @@ import br.com.infocomrobson.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
+        Gato gato = new Gato("Emma", "Malhada", 2);
         System.out.println(gato);
+        Livro livro = new Livro("COdes...", 300);
+        System.out.println(livro);
         /*int a = 5;
         int b = 13;
         System.out.println("Hello world! "+(a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro() {
+    }
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
